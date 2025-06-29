4b833216e368e75aeebbd45eaf84f932eca127d6
Yury Dubinin <yury.dubinin@gmail.com>
2025-06-29 19:41:02 +0200

    fix: update paths in index.html and vercel.json for correct asset loading

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,12 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  base: './',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      input: './index.html',
+    },
+  },
 })
