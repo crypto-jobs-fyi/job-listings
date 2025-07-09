c543b44472211ef580e681463c45184639b5518e
Yury Dubinin <yury.dubinin@gmail.com>
2025-07-09 11:17:54 +0200

    refactor: Remove crypto-companies.js as part of project restructuring

--- crypto-companies.js
+++
@@ -1,8 +0,0 @@
-import './src/main.js';
-import CryptoCompanies from './src/CryptoCompanies.svelte';
-
-const app = new CryptoCompanies({
-  target: document.getElementById('app')
-});
-
-export default app;
