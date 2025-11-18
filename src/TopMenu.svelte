73aa62ea86a393da08a889fdb4feef86685f5fd8
Yury Dubinin <yury.dubinin@gmail.com>
2025-11-18 18:25:45 +0100

    style: adjust TopMenu layout for improved alignment and spacing

--- src/TopMenu.svelte
+++ src/TopMenu.svelte
@@ -54,8 +54,8 @@
     padding: 1rem 1.25rem;
     display: flex;
     align-items: center;
-    justify-content: space-between;
-    gap: 1rem;
+    justify-content: center;
+    gap: 1.5rem;
   }
   .logo {
     font-weight: 700;
